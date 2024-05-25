307a3a486f8f0f1ffb83b01a16c88523f2d5bfe1
vfsphotos <viniciusfphotographer@gmail.com>
2024-05-24 21:39:42 -0300

    mais uma pra conta

--- main/views.py
+++ main/views.py
@@ -3,7 +3,10 @@ from main.models import Todo
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'main/index.html', {})
+    todos = Todo.objects.all()
+    return render(request, 'main/index.html', {'todos':todos})
 
 def show(request, id):
-   return render(request, 'main/show.html', {})
+   todo = Todo.objects.filter(id=id).first()
+   itens = todo.items.all()
+   return render(request, 'main/show.html', {'items':itens})
